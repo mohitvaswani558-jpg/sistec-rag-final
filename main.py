47312e9abeea28c0969e04599dd92879bdbc6746
mohit <mohitvaswani558@gmail.com>
2026-06-08 11:18:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.llms import OpenAI
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
 load_dotenv()
