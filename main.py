6b1cb46415e11ec7a2c8e85f2b9d1400738fcdd2
mohit <mohitvaswani558@gmail.com>
2026-06-08 11:26:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import sys
 import os
 from dotenv import load_dotenv
 import pdfplumber
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_community.llms import OpenAI
-from langchain_core.prompts import PromptTemplate
-from langchain.chains.llm import LLMChain
+from langchain.text_splitters import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 load_dotenv()
 
